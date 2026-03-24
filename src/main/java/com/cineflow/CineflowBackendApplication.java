f259c18b4cc8f6000dfeeae17097b43a371ef0fb
Himanshu Shekhar <himanshuji0211@gmail.com>
2026-03-24 20:03:59 +0530

    Initial Spring Boot setup with MySQL integration

---
+++ src/main/java/com/cineflow/CineflowBackendApplication.java
@@ -0,0 +1,13 @@
+package com.cineflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CineflowBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CineflowBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cineflow/CineflowBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cineflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CineflowBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
